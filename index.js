05eb3111947c49c051916fe34b4b99520e3e0e3b
lushijie <lushijie03@meituan.com>
2019-04-08 18:03:30 +0800

    babel7

--- index.js
+++ index.js
@@ -1,67 +1,23 @@
-var plugins = {
-  //es2015
-  'transform-es2015-modules-commonjs': [require('babel-plugin-transform-es2015-modules-commonjs'), {
-    loose: true,
-    strict: false,
-    noInterop: false
-  }],
-  //es2016
-  'transform-exponentiation-operator': require('babel-plugin-transform-exponentiation-operator'),
-  //es2017
-  'syntax-trailing-function-commas': require('babel-plugin-syntax-trailing-function-commas'),
-  'transform-async-to-generator': require('babel-plugin-transform-async-to-generator'),
-  'transform-runtime': [require('babel-plugin-transform-runtime'), {
-    helpers: false, // defaults to true
-    polyfill: false, // defaults to true
-    regenerator: true, // defaults to true
-    moduleName: 'babel-runtime' // defaults to 'babel-runtime'
-  }],
-  'transform-object-rest-spread': [require('babel-plugin-transform-object-rest-spread'), {
-    useBuiltIns: true
-  }]
-};
+const deepExtend = require('deep-extend');
+const { declare } = require('@babel/helper-plugin-utils');
 
-function extend() {
-  var args = [].slice.call(arguments);
-  var target = args.shift();
-  for(var i=0;i<args.length;i++) {
-    for(var k in args[i]) {
-      target[k] = args[i][k];
-    }
+module.exports = declare((api, options) => {
+  api.assertVersion('^7.0.0');
+
+  const defaultPresetOptions = {
+    targets: { node: '6' },
+    modules: 'auto',
+    debug: false,
+    useBuiltIns: 'usage',
+    corejs: 2,
   }
-  return target;
-}
-
-function preset(context, opts) {
-  opts = opts || {};
-  var pluginNames = Object.keys(plugins);
-  var commonOpts = Object.keys(opts).filter(function(o) {
-    return pluginNames.indexOf(o) === -1;
-  });
 
   return {
-    plugins: pluginNames.map(function(pluginName) {
-      var plugin = plugins[pluginName];
-      if( !Array.isArray(plugin) ) {
-        plugin = [plugin, {}];
-      }
-
-      if(opts[pluginName] === false) {
-        return undefined;
-      }
-
-      var pluginOpt = plugin[1];
-      plugin[1] = extend({}, pluginOpt, commonOpts, opts[pluginName]);
-      return plugin;
-    }).filter(function(plugin) { return plugin; })
+    presets: [
+      [require('@babel/preset-env'), deepExtend({}, defaultPresetOptions, options)],
+    ],
+    plugins: [
+      [require('@babel/plugin-transform-runtime')],
+    ],
   };
-};
-
-module.exports = preset({});
-
-Object.defineProperty(module.exports, 'buildPreset', {
-  configurable: true,
-  writable: true,
-  enumerable: false,
-  value: preset,
-})
+});
